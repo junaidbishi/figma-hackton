8e32be7a1277f54f9e34bf38f3451352b29eebfc
muhammad junaid <junaidbishi02@gmail.com>
2025-01-04 14:07:49 -0800

    navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import Navbar from "./components/navbar";
 import Header from "./components/header";
-// import Herosection from "./components/herosection";
+import Herosection from "./components/herosection";
 
 export default function Home() {
   return (
 
       <><Navbar />
      <Header/>
+     <Herosection/>
       </>
 
 
