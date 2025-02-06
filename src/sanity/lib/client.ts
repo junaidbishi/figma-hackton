0046c0b9067428b3c1e4bf90c67a94a17de91f15
muhammad junaid <junaidbishi02@gmail.com>
2025-02-06 05:58:05 +0500

    fix deployment error

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@sanity/client'; 
 
 export const client = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
+  projectId: "bv4auy6e",
+  dataset: "production",
   apiVersion: '2025-01-16',
   useCdn: true,
 });
